dfe8b16b7491c026f000329f0dcbab3e41bdc0af
Pritish Mishra <pritishmishra579@gmail.com>
2024-05-02 15:47:47 +0530

    feat: :sparkles: streak calculation + reset logic

--- components/taskStats.tsx
+++ components/taskStats.tsx
@@ -1,23 +1,30 @@
 "use client";
 
-import { useTaskStore } from "@/lib/taskStore";
+import { useEffect } from "react";
 import { Card, CardHeader } from "@nextui-org/card";
 
+import { useTaskStore } from "@/lib/taskStore";
+
 export default function TaskStats() {
+    useEffect(() => {
+        useTaskStore.getState().resetStreak();
+    }, []);
+
     const tasks = useTaskStore((state) => state.tasks);
     const completedTasks = tasks.filter((task) => task.completed);
+    const currentStreak = useTaskStore((state) => state.currentStreak);
 
     return (
         <div className="flex w-full h-full gap-4">
             <Card className="w-full h-full" radius="md">
                 <CardHeader className="absolute z-10 top-1 flex-col !items-start">
                     <p className="font-bold tracking-widest uppercase text-large text-foreground/60">streak</p>
-                    <h4 className="font-bold text-9xl text-foreground">0</h4>
+                    <h4 className="font-bold text-9xl text-foreground">{currentStreak}</h4>
                 </CardHeader>
             </Card>
             <Card className="w-full h-full" radius="md">
                 <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <p className="font-bold tracking-widest uppercase text-large text-foreground/60">Completed task</p>
+                    <p className="font-bold tracking-widest uppercase text-large text-foreground/60">Completed tasks</p>
                     <h4 className="font-bold text-9xl text-foreground">{completedTasks.length}</h4>
                 </CardHeader>
             </Card>
--- lib/taskStore.ts
+++ lib/taskStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { isToday, subDays } from 'date-fns'
 import { persist } from 'zustand/middleware'
 
 type Task = {
@@ -13,6 +14,8 @@ type TaskState = {
     tasks: Task[]
 
     selectedDate: string
+
+    currentStreak: number
 }
 
 type TaskActions = {
@@ -22,6 +25,8 @@ type TaskActions = {
     toggleTask: (id: string) => void
 
     changeSelectedDate: (date: string) => void
+
+    resetStreak: () => void
 }
 
 export const useTaskStore = create<TaskState & TaskActions>()(persist(set => ({
@@ -29,10 +34,32 @@ export const useTaskStore = create<TaskState & TaskActions>()(persist(set => ({
     addTask: task => set(state => ({ tasks: [...state.tasks, task] })),
     updateTask: (id, name) => set(state => ({ tasks: state.tasks.map(task => task.id === id ? { ...task, name } : task) })),
     removeTask: id => set(state => ({ tasks: state.tasks.filter(task => task.id !== id) })),
-    toggleTask: id => set(state => ({ tasks: state.tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task) })),
+    toggleTask: id => set(state => {
+        const newState = { tasks: state.tasks.map(task => task.id === id ? { ...task, completed: !task.completed } : task) };
+        const completedToday = newState.tasks.filter(task => task.completed && isToday(task.date)).length;
+        if (completedToday == 1) {
+            const streak = state.currentStreak + 1;
+            set({ currentStreak: streak });
+        }
+
+        return newState;
+    }),
 
     selectedDate: new Date().toISOString(),
-    changeSelectedDate: date => set({ selectedDate: date })
+    changeSelectedDate: date => set({ selectedDate: date }),
+
+    currentStreak: 0,
+    resetStreak: () => set(state => {
+        const yesterday = subDays(new Date(), 1);
+        const completedToday = state.tasks.filter(task => task.completed && isToday(task.date)).length;
+        const completedYesterday = state.tasks.filter(task => task.completed && (new Date(task.date).getDate() === yesterday.getDate())).length;
+
+        if (completedYesterday === 0 && completedToday === 0) {
+            return { currentStreak: 0 };
+        }
+
+        return state;
+    }),
 }),
     { name: 'taskStore' }
 ))
